a3d9082fef95c7a952a39f6f6809a6275da1e1c9
Andréas Anemyr <aa223ig@student.lnu.se>
2014-10-01 22:57:57 +0200

    First commit of lab 2.1 C

---
+++ 1DV402.S2.L1C/1DV402.S2.L1C/SecretNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1C
+{
+    class SecretNumber
+    {
+
+        private GuessedNumber[] _guessedNumbers;
+        private int? _number;
+        public const int NumberOfGuesses = 7;
+
+        public bool CanMakeGuess{
+
+        }
+
+
+    }
+}
